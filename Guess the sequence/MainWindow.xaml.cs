5e28641190e550f4c30686d6fb34a0777b7cdb5f
Flamy <27062841+f1amy@users.noreply.github.com>
2017-11-10 03:27:42 +0500

    Code improvements

--- Guess the sequence/MainWindow.xaml.cs
+++ Guess the sequence/MainWindow.xaml.cs
@@ -12,13 +12,13 @@ namespace Guess_the_sequence
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<Button> button_array = new List<Button>();
-        private List<int> current_sequence_array = new List<int>();
-        private List<SolidColorBrush> brushes_array = new List<SolidColorBrush>()
+        private List<Button> ButtonList = new List<Button>();
+        private List<int> CurrentSequenceList = new List<int>();
+        private List<SolidColorBrush> BrushesList = new List<SolidColorBrush>()
         {
             Brushes.Red, Brushes.Orange, Brushes.Yellow, Brushes.Green, Brushes.Blue, Brushes.Purple
         };
-        private List<SolidColorBrush> dark_brushes_array = new List<SolidColorBrush>()
+        private List<SolidColorBrush> DarkBrushesList = new List<SolidColorBrush>()
         {
             new BrushConverter().ConvertFromString("#FF7F0000") as SolidColorBrush, //darkRed
             new BrushConverter().ConvertFromString("#FF805700") as SolidColorBrush, //darkOrange
@@ -27,159 +27,159 @@ namespace Guess_the_sequence
             new BrushConverter().ConvertFromString("#FF000055") as SolidColorBrush, //darkBlue
             new BrushConverter().ConvertFromString("#FF3C003C") as SolidColorBrush  //darkPurple
         };
-        private int current_guess_block = 1, current_score = 0, seq_max = 6;
+        private int CurrentGuessBlock = 1, CurrentScore = 0, SequenceMaximum = 6;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            button_array.Add(button1_red);
-            button_array.Add(button2_orange);
-            button_array.Add(button3_yellow);
-            button_array.Add(button4_green);
-            button_array.Add(button5_blue);
-            button_array.Add(button6_purple);
+            ButtonList.Add(RedButton1);
+            ButtonList.Add(OrangeButton2);
+            ButtonList.Add(YellowButton3);
+            ButtonList.Add(GreenButton4);
+            ButtonList.Add(BlueButton5);
+            ButtonList.Add(PurpleButton6);
         }
 
-        private void button_change_color(int button_number, bool enable)
+        private void ButtonChangeColor(int ButtonNumber, bool EnableMode)
         {
-            if (enable == true)
+            if (EnableMode == true)
             {
-                button_array[button_number].Background = brushes_array[button_number];
+                ButtonList[ButtonNumber].Background = BrushesList[ButtonNumber];
             }
-            else if (enable == false)
+            else if (EnableMode == false)
             {
-                button_array[button_number].Background = dark_brushes_array[button_number];
-            }
-        }
-
-        private void create_new_sequence()
-        {
-            Random rnd = new Random();
-
-            current_sequence_array.Clear();
-            seq_max = rnd.Next(6, 10 + 1);
-
-            for (int i = 0; i < seq_max; i++)
-            {
-                current_sequence_array.Add(rnd.Next(1, 7));
+                ButtonList[ButtonNumber].Background = DarkBrushesList[ButtonNumber];
             }
         }
 
-        private async void play_sequence()
+        private void CreateNewSequence()
         {
-            foreach (Button current_element in button_array) //turn off the buttons
+            Random Random = new Random();
+
+            CurrentSequenceList.Clear();
+            SequenceMaximum = Random.Next(6, 10 + 1);
+
+            for (int i = 0; i < SequenceMaximum; i++)
             {
-                current_element.IsEnabled = false;
+                CurrentSequenceList.Add(Random.Next(1, 7));
+            }
+
+            #if DEBUG
+
+            DebugStringTextBlock.Text = "Sequence: " + string.Join(", ", CurrentSequenceList) + ".";
+
+            #endif
+        }
+
+        private async void PlaySequence()
+        {
+            foreach (Button CurrentItem in ButtonList) //turn off the buttons
+            {
+                CurrentItem.IsEnabled = false;
             }
 
             await Task.Delay(500); //wait 500 milliseconds
 
-            foreach (int current_element in current_sequence_array)
+            foreach (int CurrentItem in CurrentSequenceList)
             {
-                button_change_color(current_element - 1, true);
+                ButtonChangeColor(CurrentItem - 1, true);
                 await Task.Delay(1000); //wait 1000 milliseconds
 
-                button_change_color(current_element - 1, false);
+                ButtonChangeColor(CurrentItem - 1, false);
                 await Task.Delay(500);
             }
 
-            for (int i = 0; i < button_array.Count; i++)
+            for (int i = 0; i < ButtonList.Count; i++)
             {
-                button_change_color(i, true);
+                ButtonChangeColor(i, true);
             }
 
-            foreach (Button current_element in button_array) //turn on the buttons
+            foreach (Button CurrentItem in ButtonList) //turn on the buttons
             {
-                current_element.IsEnabled = true;
+                CurrentItem.IsEnabled = true;
             }
 
-            textBlock_information.Text = "Guess";
+            InformationTextBlock.Text = "Guess";
         }
 
-        private void game_win()
+        private void GameWon(bool isWon)
         {
-            for (int i = 0; i < button_array.Count; i++)
+            for (int i = 0; i < ButtonList.Count; i++)
             {
-                button_change_color(i, false);
+                ButtonChangeColor(i, false);
             }
 
-            foreach (Button current_element in button_array) //turn off the buttons
+            foreach (Button CurrentItem in ButtonList) //turn off the buttons
             {
-                current_element.IsEnabled = false;
+                CurrentItem.IsEnabled = false;
             }
 
-            button_start.IsEnabled = true;
-            current_guess_block = 1;
-            current_score += 100 * seq_max;
-            textBlock_information.Text = "Correct";
-            textBlock_score.Text = "Score: " + current_score.ToString();
+            CurrentGuessBlock = 1;
+            StartButton.IsEnabled = true;
+
+            if (isWon == true)
+            {
+                CurrentScore += 100 * SequenceMaximum;
+            }
+            else if (isWon == false)
+            {
+                if (CurrentScore > 50 * SequenceMaximum)
+                {
+                    CurrentScore -= 50 * SequenceMaximum;
+                }
+                else
+                {
+                    CurrentScore = 0;
+                }
+            }
+
+            InformationTextBlock.Text = isWon == true ? "Correct" : "Wrong";
+            ScoreTextBlock.Text = "Score: " + CurrentScore.ToString();
+
+            #if DEBUG
+
+            DebugStringTextBlock.Text = string.Empty;
+
+            #endif
         }
 
-        private void game_fail()
+        private void StartButton_Click(object sender, RoutedEventArgs e)
         {
-            for (int i = 0; i < button_array.Count; i++)
-            {
-                button_change_color(i, false);
-            }
+            CreateNewSequence();
 
-            foreach (Button current_element in button_array) //turn off the buttons
-            {
-                current_element.IsEnabled = false;
-            }
-
-            current_guess_block = 1;
-            button_start.IsEnabled = true;
-
-            if (current_score > 50 * seq_max)
-            {
-                current_score -= 50 * seq_max;
-            }
-            else
-            {
-                current_score = 0;
-            }
-
-            textBlock_information.Text = "Wrong";
-            textBlock_score.Text = "Score: " + current_score.ToString();
-        }
-
-        private void button_start_Click(object sender, RoutedEventArgs e)
-        {
-            create_new_sequence();
-
-            textBlock_information.Text = "Watch";
-            button_start.IsEnabled = false;
+            InformationTextBlock.Text = "Watch";
+            StartButton.IsEnabled = false;
             
-            play_sequence();
+            PlaySequence();
         }
 
-        private void button_all_Click(int button_index)
+        private void AnyButton_Click(int ButtonNumber)
         {
-            if (current_sequence_array[current_guess_block - 1] == button_index && current_guess_block == current_sequence_array.Count)
+            if (CurrentSequenceList[CurrentGuessBlock - 1] == ButtonNumber && CurrentGuessBlock == CurrentSequenceList.Count)
             {
-                game_win();
+                GameWon(true);
             }
-            else if (current_sequence_array[current_guess_block - 1] != button_index)
+            else if (CurrentSequenceList[CurrentGuessBlock - 1] != ButtonNumber)
             {
-                game_fail();
+                GameWon(false);
             }
             else
             {
-                current_guess_block++;
+                CurrentGuessBlock++;
             }
         }
 
-        private void button1_red_Click(object sender, RoutedEventArgs e) => button_all_Click(1);
+        private void RedButton1_Click(object sender, RoutedEventArgs e) => AnyButton_Click(1);
 
-        private void button2_orange_Click(object sender, RoutedEventArgs e) => button_all_Click(2);
+        private void OrangeButton2_Click(object sender, RoutedEventArgs e) => AnyButton_Click(2);
 
-        private void button3_yellow_Click(object sender, RoutedEventArgs e) => button_all_Click(3);
+        private void YellowButton3_Click(object sender, RoutedEventArgs e) => AnyButton_Click(3);
 
-        private void button4_green_Click(object sender, RoutedEventArgs e) => button_all_Click(4);
+        private void GreenButton4_Click(object sender, RoutedEventArgs e) => AnyButton_Click(4);
 
-        private void button5_blue_Click(object sender, RoutedEventArgs e) => button_all_Click(5);
+        private void BlueButton5_Click(object sender, RoutedEventArgs e) => AnyButton_Click(5);
 
-        private void button6_purple_Click(object sender, RoutedEventArgs e) => button_all_Click(6);
+        private void PurpleButton6_Click(object sender, RoutedEventArgs e) => AnyButton_Click(6);
     }
 }
